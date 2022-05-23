b528ad5ce57fda94dc92b643009a57981e452163
Sebastián Gómez Graciano <juansegomez40@gmail.com>
2022-05-23 18:12:20 -0500

    adding method for cutting files

--- cypress/integration/upload-download.spec.ts
+++ cypress/integration/upload-download.spec.ts
@@ -13,7 +13,7 @@ describe("the user navigates", () => {
 
   it("should upload file and verify the file title", () => {
     fileUpload = "terminal_input_screenshot.PNG";
-    text = "terminal_input_screenshot";
+    text = fileUpload.slice(0, -4);
     // Arrange
     uploadPage.visitUploadPage();
     // Action
